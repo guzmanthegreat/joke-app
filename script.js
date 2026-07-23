3a955501a530ab6d6c1351603d6709bc883bb8cd
Josh Guzman <joshguzmanx7@gmail.com>
2026-07-23 12:07:58 -0700

    Changed background color to blue. also now hides punchline button after its pressed

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
-    const punchlineButton = document.getElementById("punchlineButton")
-    const jokeSetup = document.getElementById("jokeSetup")
-    const jokePunchline = document.getElementById("jokePunchline")
-    const newJokeButton = document.getElementById("newJokeButton")
-    const audio = document.getElementById("punchlineAudio")
-    let punchline = "";
-       
-    //calls api and returns joke
-    async function getData() {
-        try {
-            // Clear the previous joke while loading a new one
-            jokeSetup.textContent = "Loading...";
-            jokePunchline.textContent = "";
+const punchlineButton = document.getElementById("punchlineButton");
+const jokeSetup = document.getElementById("jokeSetup");
+const jokePunchline = document.getElementById("jokePunchline");
+const newJokeButton = document.getElementById("newJokeButton");
+const audio = document.getElementById("punchlineAudio");
+const newJokeButtonText = document.getElementById("newJokeButtonText");
+let punchline = "";
 
-            //api call
-            const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+//calls api and returns joke
+async function getData() {
+  try {
+    //clear the previous joke while loading a new one
+    jokeSetup.textContent = "Loading...";
+    jokePunchline.textContent = "";
+    punchlineButton.hidden = false;
 
-            if (!response.ok) {
-                throw new Error("Error during request")
-            }
+    //api call
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke",
+    );
 
-            const data = await response.json();
-            
-            jokeSetup.textContent = data.setup;
-            punchline = data.punchline;
-            
-        }   
-        catch (error) {
-            console.error(error);
-            jokeSetup.textContent = "Could not load a joke. Try again later."
-        }
+    //Prints message in console if error in http status code and jumps to catch block
+    if (!response.ok) {
+      throw new Error("Error during request");
     }
-    getData()
 
-    //reveals punchline and plays audio
-    punchlineButton.addEventListener("click", function() {
-        jokePunchline.textContent = punchline;
-        audio.play();
+    // runs if status code sucessful
+
+    //parses response from api to json js object
+    //'await' tells program to pause at this line until data is received
+    const data = await response.json();
+
+    //look at api documentation to know how to access keys (e.g. 'setup' and punchline)
+    jokeSetup.textContent = data.setup;
+    punchline = data.punchline;
+  } catch (error) {
+    console.error(error);
+    jokeSetup.textContent = "Could not load a joke. Try again later.";
+    punchlineButton.hidden = true;
+    newJokeButtonText.textContent = "Reload";
+
+    document.getElementById("newJokeButton").addEventListener("click", () => {
+      location.reload();
     });
+  }
+}
+getData();
 
-    newJokeButton.addEventListener("click", getData)
+//reveals punchline and plays audio
+punchlineButton.addEventListener("click", function () {
+  jokePunchline.textContent = punchline;
+  punchlineButton.hidden = true;
+  audio.play();
+});
+
+newJokeButton.addEventListener("click", getData);
